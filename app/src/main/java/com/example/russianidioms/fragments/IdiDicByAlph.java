675bbce4bfb4ffc267ae0a418d65ebc86aee3b23
Dilliya <dilusha.37.writer@gmail.com>
2023-04-16 23:20:35 +0400

    Basic

---
+++ app/src/main/java/com/example/russianidioms/fragments/IdiDicByAlph.java
@@ -0,0 +1,24 @@
+package com.example.russianidioms.fragments;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.russianidioms.R;
+
+
+public class IdiDicByAlph extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_idi_dic_by_alph, container, false);
+        return view;
+    }
+}
